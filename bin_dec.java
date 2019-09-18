2a764ea93f617dc6b13b3634602ba78aa1d347bd
Saurabh-1909 <55407524+Saurabh-1909@users.noreply.github.com>
2019-09-18 18:21:39 +0530

    Add files via upload

---
+++ bin_dec.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+  
+public static void main(String[] args) {
+    
+int binary;
+  
+int decimal=0,i ,x=1;
+  
+Scanner in= new Scanner(System.in);
+  
+System.out.print1("Binary number: ");
+  
+binary= in.nextInt();
+  
+while(binary !=0){
+    
+i=binary % 10;
+    
+decimal= decimal+ i*x;
+    
+x= x*2;
+    
+binary=binary/10;
+    
+  
+}
+  
+System.out.print("decimal number: "+decimal);
+ 
+}
+
+}
---
+++ dectooct.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+
+class Main {
+  public static void main(String[] args) {
+
+int dec_num, i=1, j;
+  
+int oct_num[]= new int[100];
+  
+Scanner in = new Scanner(System.in);
+  
+System.out.print("Decimal number: ");
+  
+dec_num= in.nextInt();
+  
+while(dec_num> 0){
+    
+oct_num[i]= dec_num%8;
+    
+dec_num= dec_num/8;
+    
+i++;
+  }
+  
+System.out.print("Octal number: ");
+
+ 
+for(j=i-1;j>0;j--)
+ {
+   
+System.out.print(oct_num[j]);
+ }
+ 
+System.out.print("\n");
+
+}
+
+}
